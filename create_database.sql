b5e85195c7b9c62122247972ec3a69fbdd9a2af6
Tuncay dinler <63648462+WormerKing@users.noreply.github.com>
2024-03-14 15:42:58 +0300

    Update create_database.sql

--- create_database.sql
+++ create_database.sql
@@ -295,13 +295,11 @@ CREATE TABLE `Backend_Project`.`products_logs` (
 	`after_description` TEXT,
 	`after_rank` INT  ,
 	`after_is_active` BOOLEAN,
-=======
 	`img_url` VARCHAR(50) NOT NULL ,
 	`title` VARCHAR(50) NOT NULL ,
 	`description` TEXT NOT NULL ,
 	`rank` INT NOT NULL ,
 	`is_active` BOOLEAN NOT NULL ,
->>>>>>> 7e93a464d4d9b6658bb7bf8ca33e239c9f515187
 	`created_at` TIMESTAMP NOT NULL ,
 	PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
